a8d1571f02c29e0fd3a57197dedf4aff033c8a67
NasaHack <proah.nasahack@gmail.com>
2025-04-03 02:08:27 +0000

    refactor: adds type check for input API_URL

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { network } from "./utils";
+import { network, logger } from "./utils";
 
 const API_URL: string = process.argv[2];
 
-network(API_URL);
+const init = () => {
+  if (!API_URL) return logger.error("Api URL is required");
+  network(API_URL.trim());
+};
+
+init();
